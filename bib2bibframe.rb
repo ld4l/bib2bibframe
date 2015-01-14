913163d98336ca71bfce5f73d778fcce85711e27
Rebecca Younes <rjy7@lib-c02mjedqfd57.library.cornell.edu>
2015-01-14 12:15:58 -0500

    Updates to script; refactor conversion into a class

--- bib2bibframe.rb
+++ bib2bibframe.rb
@@ -1,40 +1,22 @@
 #!/usr/bin/ruby -w
 
-# Convert a comma-separated list of Cornell bib ids to bibframe
+# Convert a comma-separated list of Cornell bib ids to Bibframe RDF
 
+require_relative 'converter'
 require 'optparse'
 require 'yaml'
-
-
-def sg_or_pl(string, count)
-  count.to_s + ' ' + string + (count == 1 ? '' : 's')
-end
-
-
-log = []
-
-
-# Process configuration file
-config = YAML.load_file('conf.yml')
-if (! File.directory?(config['datadir']))
-  Dir.mkdir(config['datadir'])
-  log << "Created data directory #{config['datadir']}."
-end
-if (! File.directory?(config['logdir']))
-  Dir.mkdir(config['logdir'])
-  log << "Created log directory #{config['logdir']}."
-end
-
-
-# Assign default options
-options = {
+  
+DEFAULTS = {
   # Serializations supported by bibframe converter: 
   # rdfxml: (default) flattened RDF/XML, everything has an identifier
   # rdfxml-raw: verbose, cascaded output
   # ntriples, json, exhibitJSON
-  :format => 'rdfxml',
+  :format => 'rdfxml',  
 }
 
+config = YAML.load_file(File.join(Dir.pwd, 'conf.yml'))
+
+options = DEFAULTS
 
 # Parse options
 OptionParser.new do |opts|
@@ -49,8 +31,8 @@ OptionParser.new do |opts|
     options[:format] = f
   end  
 
-  opts.on('--ids', '=MANDATORY', String, 'Comma- or newline-separated list of bib ids') do |ids|
-    options[:bibids] = ids
+  opts.on('--ids', '=MANDATORY', String, 'Comma-, pipe-, or newline-separated list of bib ids') do |ids|
+    options[:bibids] = ids.split(%r{,\s*|\n})
   end  
   
   opts.on_tail('-h', '--help', 'Show this message') do
@@ -59,102 +41,17 @@ OptionParser.new do |opts|
   end
   
 end.parse!
-
-
-# baseuri option overwrites config setting
-baseuri = options.has_key?(:baseuri) ? options[:baseuri] : config['baseuri']
-
-
-# Create directories to store output
-datetime = Time.now.strftime('%Y%m%d-%H%M%S')
-datadir = File.join(config['datadir'], datetime)
-Dir.mkdir(datadir)
-xmldir = File.join(datadir, 'marcxml') 
-Dir.mkdir(xmldir)
-rdfdir = File.join(datadir, 'bibframe')
-Dir.mkdir(rdfdir)
-
-# Allow either comma- or newline-delimited list of bibids 
-bibids = options[:bibids].split(%r{,\s*|\n})
-
-# Set bibframe file extension based on serialization
-rdfext = '.' +
-  case options[:format]
-    when 'rdfxml', 'rdfxml-raw'
-      'rdf'
-    when 'json', 'extendedJSON'
-      'json'
-    when 'ntriples',
-      'nt'
-  end
   
-    
-# Process the conversions
-id_count = 0
-records = []
-no_records = []
+# Commandline options take precedence over default options and config settings.
+config.merge! options
 
-bibids.each do |id|
+# Symbolize keys
+config = config.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
 
-  id_count += 1
-  
-  # Retrieve the marcxml from the Voyager catalog. Include timestamp.
-  marcxml = `curl -s http://newcatalog.library.cornell.edu/catalog/#{id}.marcxml`
-  if (! marcxml.start_with?("<record"))
-    # log << "WARNING: No record found for bib id #{id}."
-    no_records << id
-    next
-  end
-
-  records << id
-  
-  # Wrap in <collection> tag. Doesn't make any difference in the bibframe of a
-  # single record, but is needed to process multiple records into a single file,
-  # so just add it generally.
-
-  marcxml = marcxml.gsub(/<record xmlns='http:\/\/www.loc.gov\/MARC21\/slim'>/,
-  "<?xml version='1.0' encoding='UTF-8'?><collection xmlns='http://www.loc.gov/MARC21/slim'>\n
-  <record>") 
-  marcxml << "</collection>"
-  
-  # Pretty print the unformatted marcxml for display purposes
-  marcxml = `echo "#{marcxml}" | xmllint --format -`
-  
-  xmlfile = File.join(xmldir, id + '.xml')
-  File.write(xmlfile, marcxml)     
-
-  rdffile = File.join(rdfdir, id + rdfext)
-    
-  saxon = File.join('lib', 'saxon', 'saxon9he.jar')
-  xquery = File.join('lib', 'marc2bibframe', 'xbin', 'saxon.xqy') 
-
-  command = "java -cp #{saxon} net.sf.saxon.Query #{xquery} marcxmluri=#{xmlfile} baseuri=#{baseuri} serialization=#{options[:format]} > #{rdffile}"
-  system(command)
-  
-  # TODO - for app - return the marcxml and bibframe to the application 
-  # Make a config option, so don't maintain 2 versions of the script?
-  
-end
+converter = Converter.new(config)
+converter.convert
+converter.log
 
 
-# Write the log
-totals_log = "#{sg_or_pl('bib id', id_count)} processed."
 
-records_log = "#{sg_or_pl('record', records.length)} found and converted to bibframe"
-if records.length > 0
-  records_log << ': ' + records.join(', ')
-end
-records_log << '.'
 
-no_records_log = "#{sg_or_pl('bib id', no_records.length)} without a corresponding bib record"
-if no_records.length > 0
-  no_records_log << ': ' + no_records.join(', ')
-end
-no_records_log << '.'
-
-log << [ totals_log, records_log, no_records_log ]
-
-logfile = File.join(config['logdir'], datetime + '.log')
-File.open(logfile, 'w') do |file|
-  log.each { |line| file.puts line }
-end
---
+++ converter.rb
@@ -0,0 +1,145 @@
+require 'fileutils'
+
+class Converter
+
+  # Set variables and create directories common to all bibids in the conversion
+  # request
+  def initialize config
+
+    config.each {|k,v| instance_variable_set("@#{k}",v)}
+
+    @log = {
+      :message => [],
+      :records => [],
+      :no_records => [],
+    }  
+
+    @rdfext = get_rdf_ext 
+      
+    datetime = Time.now.strftime('%Y%m%d-%H%M%S')
+    @logfile = File.join(@logdir, datetime + '.log')
+    @datadir = File.join(@datadir, datetime)      
+    
+    @saxon = File.join('lib', 'saxon', 'saxon9he.jar')
+    @xquery = File.join('lib', 'marc2bibframe', 'xbin', 'saxon.xqy') 
+    # Non-rdfxml formats require an additional parameter to the LC converter
+    @method = (@format == 'ntriples' || @format == 'json') ? "!method=text" : ''
+    
+    create_data_directories
+  end
+
+  def convert
+
+    # Process the conversions
+    @bibids.each do |id|
+      xmlfile = bibid_to_marcxml id  
+      if xmlfile     
+        marcxml_to_bibframe id, xmlfile
+      end
+    end    
+  end
+
+  # Write the log to file
+  def log
+      
+    totals_log = "#{sg_or_pl('bib id', @bibids.length)} processed."
+    
+    records_log = "#{sg_or_pl('record', @log[:records].length)} found and converted to bibframe"
+    if @log[:records].length > 0
+      records_log << ': ' + @log[:records].join(', ')
+    end
+    records_log << '.'
+    
+    no_records_log = "#{sg_or_pl('id', @log[:no_records].length)} without a bib record"
+    if @log[:no_records].length > 0
+      no_records_log << ': ' + @log[:no_records].join(', ')
+    end
+    no_records_log << '.'
+    
+    @log[:message] << [ totals_log, records_log, no_records_log ]
+    
+    if (! File.directory?(@logdir) )
+      puts 'creating logdir ' + @logdir
+      Dir.mkdir @logdir
+      @log[:message] << "Created log directory #{@logdir}."
+    end
+    
+    File.open(@logfile, 'w') do |file|
+      @log[:message].each { |line| file.puts line }
+    end 
+  end
+    
+  private
+
+    def create_data_directories
+   
+      if (! File.directory?(@datadir) )
+        FileUtils.makedirs @datadir
+        @log[:message] << "Created data directory #{@datadir}."
+      end
+        
+      @xmldir = File.join(@datadir, 'marcxml') 
+      FileUtils.makedirs @xmldir unless File.directory? @xmldir
+      
+      @rdfdir = File.join(@datadir, 'bibframe', @format)
+      FileUtils.makedirs @rdfdir unless File.directory? @rdfdir
+    end
+    
+    def get_rdf_ext
+      # Set bibframe file extension based on serialization format
+      return '.' +
+        case @format
+          when 'rdfxml', 'rdfxml-raw'
+            'rdf'
+          when 'json'
+            'json'
+          when 'ntriples',
+            'nt'
+          when 'turtle',
+            'ttl'
+        end
+    end
+    
+    # Get marcxml for the bibid and write to a file
+    def bibid_to_marcxml id
+    
+      # Retrieve the marcxml from the Voyager catalog. Include timestamp.
+      marcxml_url = File.join(@catalog, id + '.marcxml')
+      marcxml = `curl -s #{marcxml_url}`
+      if (! marcxml.start_with?("<record"))
+        @log[:no_records] << id
+        return nil
+      end
+  
+      @log[:records] << id
+  
+      # Wrap in <collection> tag. Doesn't make any difference in the bibframe of a
+      # single record, but is needed to process multiple records into a single file,
+      # so just add it generally.
+      marcxml = marcxml.gsub(/<record xmlns='http:\/\/www.loc.gov\/MARC21\/slim'>/,
+        "<?xml version='1.0' encoding='UTF-8'?><collection xmlns='http://www.loc.gov/MARC21/slim'>\n
+        <record>") 
+      marcxml << '</collection>'
+  
+      # Pretty print the unformatted marcxml for display purposes
+      marcxml = `echo "#{marcxml}" | xmllint --format -`       
+      # puts marcxml 
+      
+      # Write marcxml to file
+      xmlfile = File.join(@xmldir, id + '.xml')
+      File.open(xmlfile, 'w') { |file| file.write marcxml }
+      
+      return xmlfile
+    end
+
+    # Convert marcxml for the id to bibframe rdf and write to file
+    def marcxml_to_bibframe id, xmlfile
+      rdffile = File.join(@rdfdir, id + @rdfext)  
+      @bibframe = `java -cp #{@saxon} net.sf.saxon.Query #{@method} #{@xquery} marcxmluri=#{xmlfile} baseuri=#{@baseuri} serialization=#{@format} > #{rdffile}`
+    end
+    
+    def sg_or_pl(string, count)
+      count.to_s + ' ' + string + (count == 1 ? '' : 's')
+    end
+
+end
